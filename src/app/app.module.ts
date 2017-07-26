15143e45248ada04ad852d86efef56b5adc99dd5
manishit56 <manish.sodavadiya@thomsonreuters.com>
2017-07-25 23:11:21 -0500

    Updated main App Component with route & Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+//import { JsonEditorComponent } from './component/jsoneditor/jsoneditor.component';
+import { DemoComponent } from './demo/demo.component';
+import { Ng4JsonEditorModule } from './component/ng4-jsoneditor/ng4-jsoneditor.module';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/demo', pathMatch: 'full' },
+  { path: 'demo',  component: DemoComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng4JsonEditorModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
