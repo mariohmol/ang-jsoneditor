b7a7066463095952801b8daacdf7b27a0885bffc
Mario Mol <mariohmol@gmail.com>
2020-12-17 17:47:43 -0300

    testing validating the jsonschema

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { JsonEditorComponent, JsonEditorOptions } from '../../../ang-jsoneditor/src/public_api';
 import { FormBuilder } from '@angular/forms';
+import { schema } from './schema.value';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
@@ -58,117 +59,7 @@ export class DemoComponent implements OnInit {
 
   constructor(public fb: FormBuilder) {
     this.editorOptions = new JsonEditorOptions();
-    this.editorOptions.schema = {
-      'definitions': {},
-      '$schema': 'http://json-schema.org/draft-07/schema#',
-      '$id': 'http://example.com/root.json',
-      'type': 'object',
-      'title': 'The Root Schema',
-      'required': [
-        'randomNumber',
-        'products'
-      ],
-      'properties': {
-        'randomNumber': {
-          '$id': '#/properties/randomNumber',
-          'type': 'integer',
-          'title': 'The Randomnumber Schema',
-          'default': 0,
-          'examples': [
-            10
-          ],
-          'enum': [1, 2, 3, 4, 5, 6, 7, 8]
-        },
-        'products': {
-          '$id': '#/properties/products',
-          'type': 'array',
-          'title': 'The Products Schema',
-          'items': {
-            '$id': '#/properties/products/items',
-            'type': 'object',
-            'title': 'The Items Schema',
-            'required': [
-              'name',
-              'product'
-            ],
-            'properties': {
-              'name': {
-                '$id': '#/properties/products/items/properties/name',
-                'type': 'string',
-                'title': 'The Name Schema',
-                'default': '',
-                'examples': [
-                  'car'
-                ],
-                'pattern': '^(.*)$'
-              },
-              'product': {
-                '$id': '#/properties/products/items/properties/product',
-                'type': 'array',
-                'title': 'The Product Schema',
-                'items': {
-                  '$id': '#/properties/products/items/properties/product/items',
-                  'type': 'object',
-                  'title': 'The Items Schema',
-                  'required': [
-                    'name',
-                    'model'
-                  ],
-                  'properties': {
-                    'name': {
-                      '$id': '#/properties/products/items/properties/product/items/properties/name',
-                      'type': 'string',
-                      'title': 'The Name Schema',
-                      'default': '',
-                      'examples': [
-                        'honda'
-                      ],
-                      'pattern': '^(.*)$'
-                    },
-                    'model': {
-                      '$id': '#/properties/products/items/properties/product/items/properties/model',
-                      'type': 'array',
-                      'title': 'The Model Schema',
-                      'items': {
-                        '$id': '#/properties/products/items/properties/product/items/properties/model/items',
-                        'type': 'object',
-                        'title': 'The Items Schema',
-                        'required': [
-                          'id',
-                          'name'
-                        ],
-                        'properties': {
-                          'id': {
-                            '$id': '#/properties/products/items/properties/product/items/properties/model/items/properties/id',
-                            'type': 'string',
-                            'title': 'The Id Schema',
-                            'default': '',
-                            'examples': [
-                              'civic'
-                            ],
-                            'pattern': '^(.*)$'
-                          },
-                          'name': {
-                            '$id': '#/properties/products/items/properties/product/items/properties/model/items/properties/name',
-                            'type': 'string',
-                            'title': 'The Name Schema',
-                            'default': '',
-                            'examples': [
-                              'civic'
-                            ],
-                            'pattern': '^(.*)$'
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+    this.editorOptions.schema = schema;
 
     this.initEditorOptions(this.editorOptions);
 
@@ -177,7 +68,7 @@ export class DemoComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.data = {
+    this.showData = this.data = {
       'randomNumber': 2,
       'products': [
         {
@@ -213,8 +104,6 @@ export class DemoComponent implements OnInit {
     console.log('change:', this.editor);
     console.log('change2:', this.editorTwo);
 
-
-
     /**
      * Manual validation based on the schema 
      * if the change does not meet the JSON Schema, it will use the last data
@@ -224,6 +113,7 @@ export class DemoComponent implements OnInit {
     editorJson.validate()
     const errors = editorJson.validateSchema.errors
     if (errors && errors.length > 0) {
+      console.log('Errors found', errors)
       editorJson.set(this.showData);
     } else {
       this.showData = this.editor.get();
---
+++ src/app/demo/schema.value.ts
@@ -0,0 +1,111 @@
+export const schema = {
+    'definitions': {},
+    '$schema': 'http://json-schema.org/draft-07/schema#',
+    '$id': 'http://example.com/root.json',
+    'type': 'object',
+    'title': 'The Root Schema',
+    'required': [
+      'randomNumber',
+      'products'
+    ],
+    'properties': {
+      'randomNumber': {
+        '$id': '#/properties/randomNumber',
+        'type': 'integer',
+        'title': 'The Randomnumber Schema',
+        'default': 0,
+        'examples': [
+          10
+        ],
+        'enum': [1, 2, 3, 4, 5, 6, 7, 8]
+      },
+      'products': {
+        '$id': '#/properties/products',
+        'type': 'array',
+        'title': 'The Products Schema',
+        'items': {
+          '$id': '#/properties/products/items',
+          'type': 'object',
+          'title': 'The Items Schema',
+          'required': [
+            'name',
+            'product'
+          ],
+          'properties': {
+            'name': {
+              '$id': '#/properties/products/items/properties/name',
+              'type': 'string',
+              'title': 'The Name Schema',
+              'default': '',
+              'examples': [
+                'car'
+              ],
+              'pattern': '^(.*)$'
+            },
+            'product': {
+              '$id': '#/properties/products/items/properties/product',
+              'type': 'array',
+              'title': 'The Product Schema',
+              'items': {
+                '$id': '#/properties/products/items/properties/product/items',
+                'type': 'object',
+                'title': 'The Items Schema',
+                'required': [
+                  'name',
+                  'model'
+                ],
+                'properties': {
+                  'name': {
+                    '$id': '#/properties/products/items/properties/product/items/properties/name',
+                    'type': 'string',
+                    'title': 'The Name Schema',
+                    'default': '',
+                    'examples': [
+                      'honda'
+                    ],
+                    'pattern': '^(.*)$'
+                  },
+                  'model': {
+                    '$id': '#/properties/products/items/properties/product/items/properties/model',
+                    'type': 'array',
+                    'title': 'The Model Schema',
+                    'items': {
+                      '$id': '#/properties/products/items/properties/product/items/properties/model/items',
+                      'type': 'object',
+                      'title': 'The Items Schema',
+                      'required': [
+                        'id',
+                        'name'
+                      ],
+                      'properties': {
+                        'id': {
+                          '$id': '#/properties/products/items/properties/product/items/properties/model/items/properties/id',
+                          'type': 'string',
+                          'title': 'The Id Schema',
+                          'default': '',
+                          'examples': [
+                            'civic'
+                          ],
+                          'pattern': '^(.*)$'
+                        },
+                        'name': {
+                          '$id': '#/properties/products/items/properties/product/items/properties/model/items/properties/name',
+                          'type': 'string',
+                          'title': 'The Name Schema',
+                          'default': '',
+                          'examples': [
+                            'civic'
+                          ],
+                          'pattern': '^(.*)$'
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
