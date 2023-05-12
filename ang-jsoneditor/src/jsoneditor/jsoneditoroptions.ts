1d24bfc34759ceeb6eefdb0772b8fd5cea8373ce
Mario Mol <mariohmol@gmail.com>
2023-05-12 14:58:59 -0300

    #107

--- ang-jsoneditor/src/jsoneditor/jsoneditoroptions.ts
+++ ang-jsoneditor/src/jsoneditor/jsoneditoroptions.ts
@@ -39,7 +39,7 @@ export class JsonEditorOptions {
 
 
   public onNodeName: () => void;
-  public onCreateMenu: () => void;
+  public onCreateMenu: (items: Array<any>, node: object) => void;
   public onColorPicker: () => void;
 
   /**
