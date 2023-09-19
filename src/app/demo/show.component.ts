5a540ec2b6501b8aa239e3b10dc2b2aaf51f09dc
Ryan Fore <ryan.b.fore@gmail.com>
2023-09-19 16:48:24 -0500

    Upgrade to Angular 16

--- src/app/demo/show.component.ts
+++ src/app/demo/show.component.ts
@@ -1,17 +1,9 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { JsonEditorComponent, JsonEditorOptions } from '../../../ang-jsoneditor/src/public_api';
+import { Component, Input } from "@angular/core";
 
-@Component({
-  selector: 'app-show',
-  template: '<div>{{showData()}}</div>'
+@Component ({
+  selector: "app-show",
+  template: "<div>{{data | json}}</div>"
 })
 export class ShowComponent {
-
-  @Input()
   public data;
-
-  showData(){
-    return JSON.stringify(this.data, null, 2);
-  }
-
 }
