7487280ab890f0f3af9ae88a950ce53693602954
Sheik Althaf <althafmech@outlook.com>
2023-11-12 13:31:33 +0530

    refactor: migrate to angular library

--- src/app/demo/show.component.ts
+++ src/app/demo/show.component.ts
@@ -1,6 +1,9 @@
+import { JsonPipe } from "@angular/common";
 import { Component, Input } from "@angular/core";
 
 @Component ({
+  standalone: true,
+  imports: [JsonPipe],
   selector: "app-show",
   template: "<div>{{data | json}}</div>"
 })
